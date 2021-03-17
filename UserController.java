338792e8d0528ed17d3f00824a5950d8ff3ea49c
keh0812 <hi863@naver.com>
2021-03-17 10:10:57 +0900

    Add~~

---
+++ UserController.java
@@ -0,0 +1,15 @@
+@RequestMapping("/user/insert.do")
+	public String insert() {
+	logger.info("경로 : login");
+	return "user/login";
+	logger.info("경로 : insert");
+	return "user/insert";
+
+	}
+
+	@RequestMapping("/user/update.do")
+	public String update() {
+	logger.info("경로 : update");
+	return "user/update";
+
+	}
