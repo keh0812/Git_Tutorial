e20a778f193dd59f4c56c32c02342cf2052bd279
keh0812 <hi863@naver.com>
2021-03-17 11:09:00 +0900

    Add UserController [Update]

--- UserController.java
+++ UserController.java
@@ -1,7 +1,17 @@
-@RequestMapping("/user/insert.do")
+@Controller
+public class UserController {
+
+	@RequestMapping("/user/insert.do")
 	public String insert() {
 	logger.info("경로 : login");
 	return "user/login";
+	logger.info("경로 : insert");
+	return "user/insert";
+
+	}
+
+	@RequestMapping("/user/insert.do")
+	public String insert() {
 	logger.info("경로 : insert");
 	return "user/insert";
 
@@ -13,3 +23,4 @@
 	return "user/update";
 
 	}
+}
